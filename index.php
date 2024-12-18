1f29e9e804003449961135a5fa66fe33344ff059
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-18 10:39:33 -0400

    Introduçaão do código com if/Espaços em branco/Palavras reservadas

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Introdução de código com if
+ # Introdução de código com if
  echo "Opa primeira linha <br>";
  echo "segunda linha <br>";
 
@@ -7,10 +7,23 @@
     echo "Dentro do if <br>";
  }
 
- // Espaços em branco
+ # Espaços em branco
  echo                          "Espaço em branco <br>";
  echo "testando espaço <br>";
  echo 
  "Quebra de 
  linha"
  ;
+ 
+ # Palavras reservadas
+ //  function echo() {
+ //    return true;
+ //  } 
+
+ //  function while() {
+ //    return true;
+ //  }
+
+ function minhaExpressao() {
+   return true;
+ }
