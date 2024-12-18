30d36eeb2afd709ea7ff1d380b18b21e960878af
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-18 09:59:44 -0400

    Entendendo o case sensitive no php..

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
  
- echo "testando";
+ // Não é case sensitive
+ echo "testando, ";
+ eChO "testando dois, ";
+ ECHO "EAE testando três, ";
+
+ // É case sensitive
+ $nome = "Thiago";
+
+ echo $nome;
+ echo $NOME;
