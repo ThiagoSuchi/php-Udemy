4904767cd4ab2087c199c70b48797e51a5e1d7c4
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-18 10:26:02 -0400

    Introduçaão do código com if

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
- 
- // Não é case sensitive
- echo "testando, ";
- eChO "testando dois, ";
- ECHO "EAE testando três, ";
 
- // É case sensitive
- $nome = "Thiago";
+ echo "Opa primeira linha <br>";
+ echo "segunda linha <br>";
 
- echo $nome;
- echo $NOME;
+ if(5 > 2) {
+    echo "Dentro do if ";
+ }
