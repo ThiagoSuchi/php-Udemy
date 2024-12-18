298c1f6a653d28880fedf4b5ddb671dc3e64a9a1
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-18 11:02:37 -0400

    Verificando se um número é inteiro

--- tiposDeDados/index.php
+++ tiposDeDados/index.php
@@ -12,6 +12,21 @@
  $numero2 = 10;
  
  echo $numero * $numero2;
+ echo "<br>";
 
  # Verificando se o número é inteiro
+ $num = 150;
+ $num2 = 20.9;
+ 
+ if(is_int($num)){
+    echo "O $num é um número inteiro";
+ }
+ echo "<br>";
+
+ if(is_int($num2)){
+    echo "O $num2 é um número inteiro";
+ } else {
+    echo "$num2 não é um número inteiro";
+ }
+
 ?>
