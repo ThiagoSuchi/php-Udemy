f58c219afdf3d4904a460108528db027aaf4fa17
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-18 10:51:57 -0400

    Tipos de dados

---
+++ tiposDeDados/index.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ # Números inteiros
+ echo 5;
+ echo "<br>";
+ echo 5 + 10;
+ echo "<br>";
+ echo -17;
+ echo "<br>";
+
+ $numero = 6;
+ $numero2 = 10;
+ 
+ echo $numero * $numero2;
+
+ # Verificando se o número é inteiro
+?>
